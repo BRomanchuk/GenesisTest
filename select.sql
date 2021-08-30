afeb6531d18f40465a8974ae0aca6a87c0cd294b
BRomanchuk <47035610+BRomanchuk@users.noreply.github.com>
2021-08-30 18:39:31 +0300

    fix SQL task

--- select.sql
+++ select.sql
@@ -1,5 +1,5 @@
-SELECT category_name, avg_cost FROM
+SELECT category_name FROM
   (SELECT category_name, avg(cost) as avg_cost FROM
   adverts INNER JOIN costs ON adverts.advert_id = costs.advert_id
   GROUP BY category_name)
-WHERE avg_cost > 500
+WHERE avg_cost < 500
